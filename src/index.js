634bac91c161c2846300a4afff4f6d1a7128a432
michAele13 <amanda.marrero13@gmail.com>
2024-09-11 18:25:32 -0500

    Corrected forecast JS

--- src/index.js
+++ src/index.js
@@ -61,16 +61,16 @@ days.forEach(function (day) {
 forecastHtml =
 forecastHtml +
 `
-<div class="weather-forecast">
    <div class="weather-forecast-day">
-    <div class="weather-forecast-date">Thurs</div>  
+    <div class="weather-forecast-date">${day}</div>  
     <div class="weather-forecast-icon">🌤️</div> 
     <div class="weather-forecast-temperatures">
-        <div class="weather-forecast-temperature"><strong>90°</strong></div> 
+        <div class="weather-forecast-temperature">
+        <strong>90°</strong></div> 
         <div class="weather-forecast-temperature">72°</div>
         </div>
 </div>`;
-})
+});
 
 let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML = forecastHtml;
@@ -80,3 +80,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("San Marcos");
+displayForecast();
